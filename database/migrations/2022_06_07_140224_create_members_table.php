5936c0c0147d554a6a4d99883930199394b11bfb
Hilman Fauzi Herdiana <kk726627@gmail.com>
2022-06-08 16:54:55 +0700

    edit some database

---
+++ database/migrations/2022_06_07_140224_create_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->char('kode_member',6)->unique();
+            $table->string('nama');
+            $table->string('nomor')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+};
---
+++ database/migrations/2022_06_07_180240_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->char('kode_buku',6)->unique();
+            $table->string('judul');
+            $table->string('author');
+            $table->string('publisher');
+            $table->year('tahun_terbit');
+            $table->string('isbn')->unique();
+            $table->string('kategori');
+            $table->string('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
