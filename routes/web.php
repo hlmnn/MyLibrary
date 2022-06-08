70ebdfe8d2c6b5a47cf5a680562210e9f72234bc
Hilman Fauzi Herdiana <kk726627@gmail.com>
2022-06-08 16:55:06 +0700

    edit some routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardAdminController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +18,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
+
+
+// route untuk halaman login
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+
+// route untuk halaman admin dashboard
+Route::get('/dashboard', [DashboardAdminController::class, 'index'])->middleware('auth');
+Route::get('/dashboard/buku', [DashboardAdminController::class, 'listBuku'])->middleware('auth');
+Route::get('/dashboard/member', [DashboardAdminController::class, 'listMember'])->middleware('auth');
+Route::get('/dashboard/peminjaman', [DashboardAdminController::class, 'listPinjam'])->middleware('auth');
+
+
+# === route untuk halaman member ===
+Route::get('/dashboard/member/detail', [MemberController::class, 'show']);
+// route tambah member
+Route::get('/dashboard/member/tambah', [MemberController::class, 'create']);
+Route::post('/dashboard/member', [MemberController::class,'store']);
+// route update member
+Route::get('/dashboard/member/update', [MemberController::class, 'edit']);
+Route::put('/dashboard/member/{member}', [MemberController::class, 'update']);
+// route delete member
+// Route::delete('/dashboard/member/{member}', [MemberController::class, 'destroy']);
+Route::delete('/dashboard/member/{member}', [MemberController::class, 'destroy'])->name('members.destroy');
+
+
+# === route untuk halaman buku ===
+Route::get('/dashboard/buku/detail', [BookController::class, 'show']);
+// route tambah buku
+Route::get('/dashboard/buku/tambah', [BookController::class, 'create']);
+Route::post('/dashboard/buku', [BookController::class,'store']);
+// route delete buku
+Route::delete('/dashboard/buku/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
+
+// Route::get('/member/tambah-member', function () {
+//     return view('admin.member.insert', [
+//         'title' => 'Tambah Member'
+//     ]);
+// });
