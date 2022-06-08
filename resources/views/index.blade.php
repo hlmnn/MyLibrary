2a60167aa28efd7b227f00784fbe47ed00be94fd
Hilman Fauzi Herdiana <kk726627@gmail.com>
2022-06-08 16:55:59 +0700

    edit views

---
+++ resources/views/index.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>{{ $title }}</title>
+
+    <style>
+        body {
+            background: url('https://source.unsplash.com/YLSwjSy7stw') no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            background-size: cover;
+            -o-background-size: cover;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-dark p-2">
+        <div class="container-fluid mx-5 my-2">
+            <a class="navbar-brand text-light fw-bold" href="/">MyLibrary</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    {{-- <li class="nav-item">
+                        <a class="nav-link text-light" aria-current="page" href="#">Home</a>
+                    </li> --}}
+                </ul>
+                <div class="d-flex">
+                    <a href="/login"><button type="button" class="btn btn-primary">Sign In</button></a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="card border-0 shadow my-5" >
+            <div class="card-body p-5 text-center">
+                <h1 class="">Selamat Datang di MyLibrary</h1>
+                <p class="lead">Libray Management System</p>
+                <hr>
+                <p>Copyright &copy; 2022 by Hilman Fauzi Herdiana</p>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
